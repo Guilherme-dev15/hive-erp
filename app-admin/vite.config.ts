33e50dfed108eebfb11067c843b9cbef27ba40ae
guilherme-dev15 <guilherme.macedo1598@gmail.com>
2025-12-01 16:38:08 -0300

    feat/pwa

--- app-admin/vite.config.ts
+++ app-admin/vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'HivePratas ERP',
+        short_name: 'HiveERP',
+        description: 'Sistema de Gestão para Joalharia',
+        theme_color: '#343434', // Cor Carvão
+        background_color: '#f5f5f5',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
+});
--- app-catalogo/vite.config.ts
+++ app-catalogo/vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Loja Virtual', // Nome que aparece na instalação
+        short_name: 'Loja',
+        description: 'Catálogo de Produtos',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone', // Remove a barra de endereços (parece app nativa)
+        icons: [
+          {
+            src: 'pwa-192x192.png', // Você precisará adicionar esta imagem
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png', // E esta também
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
+});
