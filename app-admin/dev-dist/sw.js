f10aa65384e667486ecb96fe01ac6f8ba130dfd5
guilherme-dev15 <guilherme.macedo1598@gmail.com>
2026-02-13 17:09:31 -0300

    Subir

--- app-admin/dev-dist/sw.js
+++ app-admin/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.jr5hcridsgg"
+    "revision": "0.jioc6il9mgo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
