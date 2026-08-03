bf240e3561df24b77946528cbb9cf098b19d2c34
guilherme-dev15 <guilherme.macedo1598@gmail.com>
2026-08-03 16:02:26 -0300

    refactor: Remover arquivos JSX compilados obsoletos da /src

--- app-admin/src/types/index.jsx
+++
@@ -1,5 +0,0 @@
-"use strict";
-// ============================================================================
-// TYPE DEFINITIONS (SINGLE SOURCE OF TRUTH)
-// ============================================================================
-Object.defineProperty(exports, "__esModule", { value: true });
