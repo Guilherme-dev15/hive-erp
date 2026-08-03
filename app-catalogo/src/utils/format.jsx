7c9ad75b5b684abf2450594521a5f1e2eac6c026
guilherme-dev15 <guilherme.macedo1598@gmail.com>
2026-08-03 16:02:26 -0300

    refactor: Remover arquivos JSX compilados obsoletos da /src

--- app-catalogo/src/utils/format.jsx
+++
@@ -1,4 +0,0 @@
-export var formatCurrency = function (value) {
-    var amount = value !== null && value !== void 0 ? value : 0;
-    return amount.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-};
