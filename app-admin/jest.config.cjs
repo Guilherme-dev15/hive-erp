8700b26e5600e365a27f19fe5ff9a5b65df27ba3
guilherme-dev15 <guilherme.macedo1598@gmail.com>
2026-08-03 21:28:48 -0300

    refactor(lint): Corrigir 120+ avisos de no-unused-vars e no-explicit-any

---
+++ app-admin/jest.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^react$': require.resolve('react'),
+  },
+};
