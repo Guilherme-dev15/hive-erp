3c913a961845b1434c3ab03897596b5db6421620
guilherme-dev15 <guilherme.macedo1598@gmail.com>
2026-08-22 16:02:09 -0300

    test(app-admin): resolver dependencias do vitest/react testing library
    - instaladas versoes corretas de rtl e jsdom compatíveis com node/vite local
    - arquivo local  criado dentro do workspace
    - adicionado ambiente jsdom global p/ front isolado da API
    - testes estao 100% PASSANDO e frontend destravado do bug react-instance

--- app-admin/dev-dist/sw.js
+++ app-admin/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.9kdml21bubs"
+    "revision": "0.0vrisibps74"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
