8f6ea5c94f35cfb55159c76f5316034ba7009300
guilherme-dev15 <guilherme.macedo1598@gmail.com>
2026-08-23 22:47:13 -0300

    test(orders): adiciona testes da maquina de estado de transicao

--- nest-api/src/orders/orders.service.spec.ts
+++ nest-api/src/orders/orders.service.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrdersService } from './orders.service';
 import { PrismaService } from '../prisma/prisma.service';
+import { OrderStatus } from '@prisma/client';
+import { BadRequestException } from '@nestjs/common';
 
 describe('OrdersService', () => {
   let service: OrdersService;
@@ -19,5 +21,17 @@ describe('OrdersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('Máquina de Estados (Valid Transitions)', () => {
+    it('deve permitir transição de AGUARDANDO_PAGAMENTO para EM_PRODUCAO', () => {
+      const valid = service['getValidTransitions'](OrderStatus.AGUARDANDO_PAGAMENTO);
+      expect(valid).toContain(OrderStatus.EM_PRODUCAO);
+    });
+
+    it('NÃO deve permitir transição de CANCELADO para qualquer outro status', () => {
+      const valid = service['getValidTransitions'](OrderStatus.CANCELADO);
+      expect(valid.length).toBe(0);
+    });
   });
 });
