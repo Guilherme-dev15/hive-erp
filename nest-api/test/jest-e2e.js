556bd1fa2e9b099333be5fd28679fa2c4412a095
guilherme-dev15 <guilherme.macedo1598@gmail.com>
2026-08-29 00:17:48 -0300

    fix(ci): estabiliza timeout e conexao do banco nos testes e2e

--- nest-api/test/jest-e2e.js
+++ nest-api/test/jest-e2e.js
@@ -2,6 +2,8 @@ module.exports = {
   moduleFileExtensions: ["js", "json", "ts"],
   rootDir: ".",
   testEnvironment: "node",
+  testTimeout: 30000,
+  setupFilesAfterEnv: ["<rootDir>/setup-e2e.js"],
   testRegex: ".e2e-spec.ts$",
   transform: {
     "^.+\.(t|j)s$": "ts-jest"
