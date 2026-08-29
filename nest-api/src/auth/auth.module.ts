a312c2f9a197f322e01facdc854d51d74d1c841b
guilherme-dev15 <guilherme.macedo1598@gmail.com>
2026-08-29 20:11:42 -0300

    fix(auth): decoupling React from Firestore allowing PostgreSQL authentication

--- nest-api/src/auth/auth.module.ts
+++ nest-api/src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuthGuard } from './auth.guard';
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [PrismaModule],
+  controllers: [AuthController],
   providers: [AuthGuard],
   exports: [AuthGuard],
 })
