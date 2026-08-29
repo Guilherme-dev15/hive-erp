fb5a644d0bf302c38c07327e367eb84494ddd46b
guilherme-dev15 <guilherme.macedo1598@gmail.com>
2026-08-28 21:39:59 -0300

    chore(infra): remove backend legado (express) de uma vez por todas (Fase 5.7 Finalizada)

--- api/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  clearMocks: true,
-  // Não é mais necessário, pois o Jest encontra a pasta __mocks__ automaticamente
-  // setupFilesAfterEnv: ['./__tests__/setup.js'],
-};
